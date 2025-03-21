26d61b733ae9fe30b714bb22ec550600957d244d
sahil-singh1107 <sahilpython@gmail.com>
2025-03-22 02:37:10 +0530

    added space information test

--- tests/index.test.js
+++ tests/index.test.js
@@ -15,12 +15,12 @@ describe("Authentication",  () => {
             name,
             type : "admin"
         })
-        expect(res.statusCode).toBe(200);
+        expect(res.status).toBe(200);
     })
 
     test("request fails if user tries to signup without clerkId", async () => {
         const res =  await axios.post(`${BACKEND_URL}/api/v1/signup`);
-        expect(res.statusCode).toBe(400);
+        expect(res.status).toBe(400);
     })
 })
 
@@ -55,7 +55,7 @@ describe("User metadata endpoints", () => {
             clerkId : clerkIdGlobal,
             avatarId
         });
-        exports(res.statusCode).toBe(400);
+        exports(res.status).toBe(400);
     })
 
     test("user can update metadata with right avatarId", async () => {
@@ -63,14 +63,14 @@ describe("User metadata endpoints", () => {
             clerkId : clerkIdGlobal,
             avatarId : avatarIdGlobal
         });
-        exports(res.statusCode).toBe(200);
+        exports(res.status).toBe(200);
     })
 
     test("user is not able to update metadata if clerkId is absent", async () => {
         const res = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`, {
             avatarId : avatarIdGlobal
         });
-        exports(res.statusCode).toBe(400);
+        exports(res.status).toBe(400);
     })
 
 
@@ -108,5 +108,151 @@ describe("user avatar information", () => {
     test("available avatars lists the recently created avatar", async () => {
         const res = await axios.get(`${BACKEND_URL}/api/v1/avatars`);
         expect(res.data.avatars.length).not.toBe(0);
+    })
+})
+
+describe("space information", () => {
+    let mapId, element1Id, element2Id, adminClerkId = "user_" + v4(), userClerkId = "user_" + v4();
+
+    beforeAll( async () =>
+    {
+
+        const imageUrl = "image.jpg";
+        const name = "sahil";
+        await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            clerkId : adminClerkId,
+            imageUrl,
+            name,
+            type : "admin"
+        })
+
+        await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            clerkId : userClerkId,
+            imageUrl,
+            name,
+        })
+
+        const element1Res = await axios.post(`${BACKEND_URL}/api/v1/admin/element`, {
+            imageUrl : "dfsdfds",
+            width : 1,
+            height : 1,
+            static : true
+        }, {
+            headers : {
+                clerkId: adminClerkId
+            }
+        })
+
+        element1Id = element1Res.data.id;
+
+        const element2Res = await axios.post(`${BACKEND_URL}/api/v1/admin/element`, {
+            imageUrl : "dfsdfds",
+            width : 1,
+            height : 1,
+            static : false
+        }, {
+            headers : {
+                clerkId: adminClerkId
+            }
+        })
+
+        element2Id = element2Res.data.id;
+
+
+        const res = await axios.post(`${BACKEND_URL}/api/v1/admin/map`, {
+            thumbnail : "fdsfsdf.png",
+            dimensions : "100x200",
+            name : "something",
+            defaultElements : [{
+                elementId : element1Id,
+                x : 20,
+                y : 20
+            }, {
+                elementId:  element2Id,
+                x : 40, y : 40
+            }]
+        }, {
+            headers : {
+                clerkId: adminClerkId
+            }
+        })
+
+        mapId = res.data.id
+    })
+
+    test("user is able to create a space", async () => {
+        const res = await axios.post(`${BACKEND_URL}/api/v1/space`, {
+            name : "test",
+            dimensions : "100x200",
+            mapId
+        }, {
+            headers : {
+                clerkId : userClerkId
+            }
+        })
+
+        expect(res.data.spaceId).toBeDefined();
+    })
+
+    test("user is able to create a space without a map", async () => {
+        const res = await axios.post(`${BACKEND_URL}/api/v1/space`, {
+
+            name : "test",
+            dimensions : "100x200",
+        }, {
+            headers : {
+                clerkId : userClerkId,
+            }
+        })
+        expect(res.data.spaceId).toBeDefined();
+    })
+
+    test("user is not able to create a space without a map and dimensions", async () => {
+        const res = await axios.post(`${BACKEND_URL}/api/v1/space`, {
+            name : "test",
+        }, {
+            headers : {
+                clerkId : userClerkId,
+            }
+        })
+        expect(res.status).toBe(400);
+    })
+
+    test("user is not able to delete a space that doesnt exist", async () => {
+        const res = await axios.delete(`${BACKEND_URL}/api/v1/space/123`, {
+            headers : {
+                clerkId : userClerkId,
+            }
+        })
+        expect(res.status).toBe(400);
+    })
+
+    test("user is able to delete a space", async () => {
+
+        const res = await axios.post(`${BACKEND_URL}/api/v1/space`, {
+            name : "test",
+            dimensions : "100x200",
+            mapId
+        }, {
+            headers : {
+                clerkId : userClerkId,
+            }
+        })
+
+        const resDelete = await axios.delete(`${BACKEND_URL}/api/v1/space/${res.data.spaceId}`, {
+            headers : {
+                clerkId : userClerkId,
+            }
+        })
+        expect(resDelete.status).toBe(200);
+    })
+
+    test("user is able to get all space", async () => {
+        const res =  await axios.get(`${BACKEND_URL}/api/v1/space/all`, {
+            headers : {
+                clerkId : userClerkId,
+            }
+        })
+        expect(res.status).toBe(200)
     })
 })
