3a07236bb4b96e286fcc0a99212826d3dbe0d592
sahil-singh1107 <sahilpython@gmail.com>
2025-03-27 19:48:05 +0530

    addec hero section

--- greed-island/apps/frontend/src/app/(website)/page.tsx
+++ greed-island/apps/frontend/src/app/(website)/page.tsx
@@ -1,5 +1,6 @@
 import {currentUser} from "@clerk/nextjs/server";
 import Navbar from "@/app/(website)/_components/Navbar";
+import Hero from "@/app/(website)/_components/Hero";
 
 const Page = async () => {
 
@@ -7,6 +8,7 @@ const Page = async () => {
 
     return <div>
         <Navbar isLoggedIn={user ? true : false} />
+        <Hero/>
     </div>
 }
 
